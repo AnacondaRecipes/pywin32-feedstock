432f0eef063ec217db43448ed2407972d88aa686
ArishaMays1 <1arishamays1@gmail.com>
2025-08-06 14:56:09 -0400

    bld.bat functionality

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,19 @@ if %ERRORLEVEL% neq 0 exit 1
 echo "Copying over stray DLLS"
 if %ERRORLEVEL% neq 0 exit 1
 
+:: Copy files from pythonwin to win32 directory
+copy %PREFIX%\Lib\site-packages\pythonwin\*.pyd %PREFIX%\Lib\site-packages\win32
+if errorlevel 1 exit /b 1
+copy %PREFIX%\Lib\site-packages\pythonwin\*.dll %PREFIX%\Lib\site-packages\win32
+if errorlevel 1 exit /b 1
+copy %PREFIX%\Lib\site-packages\pythonwin\*.dll %LIBRARY_BIN%\
+if errorlevel 1 exit /b 1
+
+:: Create Library\bin directory and copy DLLs
+mkdir %PREFIX%\Library\bin
+copy %PREFIX%\Lib\site-packages\win32\*.dll %PREFIX%\Library\bin\
+if errorlevel 1 exit /b 1
+
 dir %PREFIX%\Lib\site-packages\win32\py*.dll
 copy %PREFIX%\Lib\site-packages\pywin32_system32\*.dll %PREFIX%\Lib\site-packages\win32\
 dir %PREFIX%\Lib\site-packages\win32\py*.dll
